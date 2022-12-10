b1dc0c74ea2b9a0333d15996960321f1028c731b
prashantpkc <prashantkumarpkc@gmail.com>
2022-12-10 23:50:28 +0530

    for assignment purpose

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
-const _ = require('underscore')
+const intro = require("./introduction");
+const employee = require("./employee");
+const _ = require("underscore");
 
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
+const movie1 = require("../film/movie1");
+const movie2 = require('../film/movie2')
 
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
+router.get("/test-me", function (req, res) {
+  console.log("email from introduction module", intro.myEmail);
+  intro.myFunction("Sabiha");
+  console.log("email from employee module", employee.myEmail);
 
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let result = _.first(days, 4);
+  console.log(`Result from underscore function is ${result}`);
 
-    res.send('any dummy text')
+  res.send("any dummy text");
 });
 
+router.get("/test-you", function (req, res) {
+  console.log("I am here");
+  res.send("very important text");
+});
 
-router.get('/test-you', function(req, res){
-    console.log("I am here")
-    res.send("very important text")
-})
-router.get('/movies/', function (req, res) {
-    res.send(["Harry Potter", "Narnia", "Mission Impossible", "Avengers"])
+router.get("/movies/", function (req, res) {
+  res.send(["Harry Potter", "Narnia", "Mission Impossible", "Avengers"]);
+});
+
+router.get("/movies/:indexNumber", function (req, res) {
+  res.send(["The name of movie"]);
+  console.log("the name of movie is", movie1.film1(req.params.indexNumber));
+});
+
+router.get("/films", function (req, res) {
+  res.send([
+    {
+      id: 1,
+      name: "The Shining",
+    },
+    {
+      id: 2,
+      name: "Incendies",
+    },
+    {
+      id: 3,
+      name: "Rang de Basanti",
+    },
+    {
+      id: 4,
+      name: "Finding Nemo",
+    },
+  ]);
+});
+
+router.get('/films2/:i', function (req, res) {
+  res.send(["The name of movies"]);
+  console.log(movie2.films2(req.params.i))
 })
 
 
 module.exports = router;
+
