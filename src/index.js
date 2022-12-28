077b992d3c7f846611bd80721edc07253508244d
prashantpkc <prashantkumarpkc@gmail.com>
2022-12-28 23:10:03 +0530

    assignment auth2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prashantpkc123:Prashant123@prashant.2lyameg.mongodb.net/jwt1", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,23 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
 
-    next()
-}
+module.exports.authenticate = async (req, res, next) => {
+  let token = req.headers["x-auth-token"];
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+  jwt.verify(
+    token,
+    "functionup-californium-very-very-secret-key",
+    (err, decodedToken) => {
+      if (err)
+        return res.status(401).send({ staus: false, message: err.message });
+      req.id = decodedToken.userId;
+      next();
+    }
+  );
+};
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+exports.authorise = async (req, res, next) =>{
+  let userId = req.params.userId;
+  let id = req.id;
+  if (userId !== id) return res.send({ status: false, msg: "Invalid user" });
+  next();
+};
